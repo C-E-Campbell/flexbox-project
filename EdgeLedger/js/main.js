dfcde5268e51c60c7c612a9f2a3a0d926e753167
Charles Campbell <charles.e.campbell1989@gmail.com>
2019-04-15 06:54:51 -0400

    done for now

--- EdgeLedger/js/main.js
+++ EdgeLedger/js/main.js
@@ -12,7 +12,7 @@ function initMap() {
 
 window.addEventListener('scroll', function(){
     if(window.scrollY > 150){
-        document.querySelector('#navbar').style.opacity = 0.7;
+        document.querySelector('#navbar').style.opacity = 0.9;
     }else{
         document.querySelector('#navbar').style.opacity = 1;
     }
